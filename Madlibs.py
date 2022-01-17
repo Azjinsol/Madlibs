639987b72375555f52f1d51191a26f8b646f3093
neomunuke <96511423+neomunuke@users.noreply.github.com>
2022-01-17 13:35:30 -0800

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+noun = input("Noun: ")
+person = input("Person: ")
+
+madlib = f"{noun}} is so {adj}! It makes me so excited on it because I love to {verb}. You are like {person}!"
+
+print(madlib)
